f71d6a1a8411a7a3a6df08fc8444bae343ae115c
Yassin Fradj <yfradj@f6r2s4.clusters.42paris.fr>
2025-09-02 15:30:35 +0200

    ex00

--- cpp_06/ex00/ScalarConverter/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter/ScalarConverter.cpp
@@ -1,6 +1,8 @@
 #include "ScalarConverter.hpp"
 #include <string>
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
 
 enum type
 {
@@ -11,11 +13,18 @@ enum type
 	NO
 };
 
+static int ft_isprint(int c)
+{
+	if (c <= 31 || c >= 127)
+		return (0);
+	return (1);
+}
+
 static type detect_type(std::string str)
 {
 	bool hasDecimal = false;
 	bool isFloat = false;
-	int start = 0;
+	size_t start = 0;
 
 	if (str == "nan" || str == "nanf")
 		return FLOAT;
@@ -23,12 +32,12 @@ static type detect_type(std::string str)
 		return DOUBLE;
 	if (str == "+inff" || str == "-inff" || str == "inff")
 		return FLOAT;
-	if (str.length() == 1 && !(str[0] >= '0' && str[0] <= '9') && (str[0] >= 32 && str[0] <= 126))
+	if (str.length() == 1 && !(str[0] >= '0' && str[0] <= '9') && (ft_isprint(str[0])))
 		return CHAR;
 
 	if (str[0] == '+' || str[0] == '-')
 		start = 1;
-	for (int i = start; i < str.length(); i++)
+	for (size_t i = start; i < str.length(); i++)
 	{
 		if (str[i] == '.')
 		{
@@ -51,12 +60,71 @@ static type detect_type(std::string str)
 		return INT;
 }
 
+static void display_int(std::string str)
+{
+	int data = atoi(str.c_str());
+
+	char arg_char = static_cast<unsigned char>(data);
+	if (!ft_isprint(arg_char))
+		std::cout << "char : Non displayable" << std::endl;
+	else
+		std::cout << "char: " << arg_char << std::endl;
+
+	std::cout << "int: " << data << std::endl;
+
+	float arg_float = static_cast<float>(data);
+	std::cout << "float: " << std::fixed << std::setprecision(1) << arg_float << "f" << std::endl;
+
+	double arg_double = static_cast<double>(data);
+	std::cout << "double: " << std::fixed << std::setprecision(1) << arg_double << std::endl;
+}
+
+static void display_char(std::string str)
+{
+	(void)str;
+}
+
+static void display_float(std::string str)
+{
+	float data = atof(str.c_str());
+
+	char arg_char = static_cast<unsigned char>(data);
+	if (!ft_isprint(arg_char))
+		std::cout << "char : Non displayable" << std::endl;
+	else
+		std::cout << "char: " << arg_char << std::endl;
+		
+	int arg_int = static_cast<int>(data);
+	std::cout << "int: " << arg_int << std::endl;
+	
+	std::cout << "float: " << data << "f" << std::endl;
+	
+	double arg_double = static_cast<double>(data);
+	std::cout << "double: " << arg_double << std::endl;
+		
+}
+
+static void display_double(std::string str)
+{
+	
+}
+
 void ScalarConverter::convert(std::string str)
 {
-	type a = detect_type(str);
-	if (a == NO)
+	type t = detect_type(str);
+	if (t == NO)
 	{
 		std::cout << "Argument not valid" << std::endl;
-		return ;
+		return;
 	}
+	if (t == INT)
+		display_int(str);
+	else if (t == CHAR)
+		display_char(str);
+	else if (t == FLOAT)
+		display_float(str);
+	else if (t == DOUBLE)
+		display_double(str)
 }
+
+//pas oublier les cas particulier pour float et double, et gestion d'erreur pour atoi pour nombre trop grands, pareil pour atof
