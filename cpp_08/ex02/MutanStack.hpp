dfe21110b4423a528aa44994337cc51514e3a9ea
yassn0 <yassinfradj28@gmail.com>
2025-09-08 14:45:22 +0200

    ex02 cpp_08

--- cpp_08/ex02/MutanStack.hpp
+++ cpp_08/ex02/MutanStack.hpp
@@ -3,36 +3,48 @@
 #include <deque>
 #include <iostream>
 #include <cstdlib>
+#include <list>
+#include <stack>
 
 template <class T, class Container = std::deque<T> >
-class MutantStack
+class MutantStack : public std::stack<T, Container>
 {
 public:
-	
 	typedef T value_type;
 	typedef Container container_type;
 	typedef std::size_t size_type;
 
-	MutantStack() : _cont(Container())
+	typedef typename Container::iterator iterator;
+	typedef typename Container::const_iterator const_iterator;
+	typedef typename Container::reverse_iterator reverse_iterator;
+	typedef typename Container::const_reverse_iterator const_reverse_iterator;
+
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+	const_iterator begin() const { return this->c.begin(); }
+	const_iterator end() const { return this->c.end(); }
+	reverse_iterator rbegin() { return this->c.rbegin(); }
+	reverse_iterator rend() { return this->c.rend(); }
+	const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+	const_reverse_iterator rend() const { return this->c.rend(); }
+
+	MutantStack() : std::stack<T, Container>()
 	{
 		std::cout << "MutanStack Constructor called" << std::endl;
 	}
 	~MutantStack()
 	{
 	}
-	MutantStack(const MutantStack &copy) : _cont(copy._cont)
+	MutantStack(const MutantStack &copy) : std::stack<T, Container>(copy)
 	{
 		std::cout << "MutanStack Copy Constructor called" << std::endl;
 	}
 	MutantStack &operator=(const MutantStack &obj)
 	{
-		_cont = obj._cont;
+		std::stack<T, Container>::operator=(obj);
 		std::cout << "operator= MutanStack called" << std::endl;
 		return *this;
 	}
-	
-	
 
 private:
-	Container _cont;
 };
